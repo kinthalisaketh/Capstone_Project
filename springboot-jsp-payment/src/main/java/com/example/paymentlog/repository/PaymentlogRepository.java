894ee3c8e5a25dc5d2f48c5d81f0a04a0b45fb19
Kinthali Saketh <103805011+kinthalisaketh@users.noreply.github.com>
2025-02-21 14:53:47 +0530

    Delete springboot-jsp-payment directory

--- springboot-jsp-payment/src/main/java/com/example/paymentlog/repository/PaymentlogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.paymentlog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.paymentlog.model.Paymentlog;
-
-public interface PaymentlogRepository extends JpaRepository<Paymentlog, Integer> {}
